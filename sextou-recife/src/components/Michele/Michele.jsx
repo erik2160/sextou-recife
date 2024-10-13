01ff43b1c9b018c703429aac5ffb140857463025
Michele <michelesantana384@gmail.com>
2024-10-12 22:31:15 -0300

    criação do banner principal

--- sextou-recife/src/components/Michele/Michele.jsx
+++ sextou-recife/src/components/Michele/Michele.jsx
@@ -1,1 +1,38 @@
-// oi 
+import React, { useState, useEffect } from 'react';
+
+function Banner () {
+  const words = ['evento', 'bar', 'restaurante', 'comércio', 'local']; 
+  const [index, setIndex] = useState(0);
+  const [typing, setTyping] = useState(false);
+
+  useEffect(() => {
+    let timer = setInterval(() => {
+      setTyping(!typing);
+
+      if (typing) {
+        if (index === words.length - 1) {
+          setIndex(0);
+        } else {
+          setIndex(index + 1);
+        }
+      }
+    }, 1000);
+
+    return () => clearInterval(timer); 
+  }, [index, typing]);
+
+return (
+  <div className='banner'>
+    <div className='container'></div>
+    <h1>Encontre o melhor
+      <p>{words[index].split('').map((letter, i) => (
+        <span key={i}>
+          {letter}
+        </span>
+      ))}
+        </p>para você</h1>
+    <button type='button'>Bora sextar?</button>
+  </div>
+)};
+
+export default Banner;
