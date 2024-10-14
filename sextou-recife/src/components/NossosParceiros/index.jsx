f0fb8462cd53d440b996bf43ea383495c249d832
Erik Fernandes <erikfesilva@gmail.com>
2024-10-14 10:59:53 -0300

    feat: arquivos iniciais

---
+++ sextou-recife/src/components/NossosParceiros/index.jsx
@@ -0,0 +1,14 @@
+// import "style.css";
+
+function nossosParceiros() {
+    return (
+        <div className="parceiros">
+            <h2>Nossos Parceiros</h2>
+            <img src="partner1.png" alt="Partner 1" />
+            <img src="partner2.png" alt="Partner 2" />
+            <img src="partner3.png" alt="Partner 3" />
+        </div>
+    );
+}
+
+export default nossosParceiros;
