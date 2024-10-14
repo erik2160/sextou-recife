06d0924658a47be88fd63fdb7d1f23608e5077d7
rafaela-albuquerquel <136506049+rafaela-albuquerquel@users.noreply.github.com>
2024-10-14 19:11:48 -0300

    'cls

--- sextou-recife/src/components/NavBar/index.jsx
+++ sextou-recife/src/components/NavBar/index.jsx
@@ -5,22 +5,23 @@
 // import logo from '../NavBar/img/logo-sextou-recife.png';
 
 import "./navbar.css";
+{/*import Logo from './public/sextou-recife-logo.svg'*/}
 
 function Navbar() {
     return (
         // Anteriromente aqui, você estava chamando <Navbar> ou seja, chamando o próprio componente dentro do componente
         // o correto é usar uma div e não chamar o proprio componente
-        <div className="styles.header">
+        <div className="styles-navbar">
             <span>
                 {/* Arrume a importação da imagem para poder adicionar aqui */}
-                {/* <img src={logo} alt="logo-sextou-recife.png" /> */}
+                <img src="/public/Sextou-logo.png" alt="Logo" href="#"/>
             </span>
             <nav>
                 <a href="">Mapa</a>
                 <a href="">Eventos</a>
                 <a href="">Parceiros</a>
-                <a href="">Contato</a>
             </nav>
+            <button>Contato</button>
         </div>
     );
 }
