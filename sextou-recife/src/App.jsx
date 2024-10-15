d741de19f40f851fb293848cdada9feefd181461
Erik Fernandes <erikfesilva@gmail.com>
2024-10-14 22:26:35 -0300

    fix: componentes adicionados no app

--- sextou-recife/src/App.jsx
+++ sextou-recife/src/App.jsx
@@ -1,10 +1,19 @@
 import "./App.css";
+
+import NavBar from "./components/NavBar";
+import Banner from "./components/Banner";
+import EventosFuturos from "./components/EventosFuturos";
 import NossosParceiros from "./components/NossosParceiros";
+import Footer from "./components/Footer";
 
 function App() {
     return (
         <>
+            <NavBar />
+            <Banner />
+            <EventosFuturos />
             <NossosParceiros />
+            <Footer />
         </>
     );
 }
--- sextou-recife/src/components/NavBar/index.jsx
+++ sextou-recife/src/components/NavBar/index.jsx
@@ -1,16 +1,7 @@
-// Esse import pode ser removido
-// import React from 'react';
-
-// Essa imagem não está sendo localizada, a pasta não existe e o arquivo também não
-// import logo from '../NavBar/img/logo-sextou-recife.png';
-
-import "./navbar.css";
-
+import "./style.css";
 function Navbar() {
     return (
-        // Anteriromente aqui, você estava chamando <Navbar> ou seja, chamando o próprio componente dentro do componente
-        // o correto é usar uma div e não chamar o proprio componente
-        <div className="styles.header">
+        <div className="navbar">
             <span>
                 {/* Arrume a importação da imagem para poder adicionar aqui */}
                 {/* <img src={logo} alt="logo-sextou-recife.png" /> */}
@@ -26,5 +17,3 @@ function Navbar() {
 }
 
 export default Navbar;
-// antes estava: export default NavBar
-// O nome do export estava diferente da function
