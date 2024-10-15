23ad36d20caddd169f681ddaf67b36afe65ac0ed
vic-fmr <vfm@poli.br>
2024-10-14 21:54:02 -0300

    inicio-carrossel

--- sextou-recife/src/main.jsx
+++ sextou-recife/src/main.jsx
@@ -1,6 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+<<<<<<< Updated upstream
+=======
+import './index.css'
+import {register} from 'swiper/element/bundle'
+
+register();
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import 'swiper/css/scrollbar'
+
+>>>>>>> Stashed changes
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
