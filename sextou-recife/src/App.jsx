8a28f8c1a2d44194465ee3fbaa27bc0e919d3ab6
vic-fmr <vfm@poli.br>
2024-10-17 01:56:38 -0300

    fix: centralização carrossel

--- sextou-recife/src/App.jsx
+++ sextou-recife/src/App.jsx
@@ -4,25 +4,29 @@ import Carrossel from "./components/Carrossel";
 import EventosFuturos from "./components/EventosFuturos";
 import NossosParceiros from "./components/NossosParceiros";
 import Footer from "./components/Footer";
-import "./App.css"
+import "./App.css";
 
 function App() {
-    return (
-        <div className="main-content">
-            <header>
-                <Navbar />
-            </header>
-            <main>
-                <Banner />
-                <Carrossel />
-                <EventosFuturos />
-                <NossosParceiros />
-            </main>
-            <footer>
-                <Footer />
-            </footer>
-        </div>
-    );
+  return (
+    <div className="main-content">
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <Banner />
+        <article>
+          <Carrossel />
+        </article>
+        <article>
+          <EventosFuturos />
+        </article>
+        <NossosParceiros />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 }
 
 export default App;
